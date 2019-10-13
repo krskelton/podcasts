73166d07f9d83c0e8761b583346068bb26dc4ae4
Kristin Skelton <kristinskelton@Kristins-MacBook-Air.local>
2019-10-13 13:10:42 -0600

    Remove fodder and replace with Podcast layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Podcasts podcasts="Podcasts:"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Podcasts from './components/Podcasts.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Podcasts
   }
 }
 </script>
