b296574e8931e3a223107b9ab82e59a731af58f3
Theresa O'Hare <theresaohare@Theresas-MacBook-Air.local>
2019-11-21 20:42:18 -0700

    fixed typo in models

--- server/models.py
+++ server/models.py
@@ -4,7 +4,7 @@ from sqlalchemy import ForeignKey
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True)
-    password = db.Column(db.String(128), nullable=False))
+    password = db.Column(db.String(128), nullable=False)
     salt = db.Column(db.String(40), nullable=False)
 
 class Podcasts(db.Model):
