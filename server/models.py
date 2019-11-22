fc893987dc5f8264e7e90c582566f21e22e26537
Theresa O'Hare <theresaohare@Theresas-MacBook-Air.local>
2019-11-21 20:42:44 -0700

    fixed typo in models

--- server/models.py
+++ server/models.py
@@ -7,7 +7,7 @@ class Users(db.Model):
     password = db.Column(db.String(128), nullable=False)
     salt = db.Column(db.String(40), nullable=False)
 
-class Podcasts(db.Model):
+class Podcast(db.Model):
     # we want to change the name of this table
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
