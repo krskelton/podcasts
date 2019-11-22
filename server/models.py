2de958744025bbdf1ef83d37fcc3d44f1c273d21
Theresa O'Hare <theresaohare@Theresas-MacBook-Air.local>
2019-11-21 20:34:34 -0700

    Added Users, Playlists, PlaylistItems, and History tables to models

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from sql_alchemy_db_instance import db
+from sqlalchemy import ForeignKey
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,10 +7,23 @@ class Users(db.Model):
     password = db.Column(db.String(128, nullable=False))
     salt = db.Column(db.String(40), nullable=False)
 
-class Subscriptions(db.Model):
+class Podcasts(db.Model):
+    # we want to change the name of this table
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(2000))
     rss_url = db.Column(db.String(2000), nullable=False)
 
-class Favorites(db.Model)
-    
+class Playlists(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
+class PlaylistItems(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey("playlist.id"))
+    podcast_API_id = db.Column(db.Integer)
+    episode_id = db.Column(db.Integer)
+
+class History(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    episode_id = db.Column(db.Integer)
