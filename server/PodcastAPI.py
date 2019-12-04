89286ad893549a62ca5082e9182a9b0b640dfac4
Theresa O'Hare <theresaohare@Theresas-MacBook-Air.local>
2019-12-03 20:58:16 -0700

    updated PodcastAPI to add user id to database on subscribe

--- server/PodcastAPI.py
+++ server/PodcastAPI.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, session
 from sql_alchemy_db_instance import db
-from models import Podcast
+from models import Podcast, Users
 import requests
 
 podcast_api = Blueprint('podcast_api', __name__)
@@ -14,6 +14,9 @@ def serve_all_subscriptions():
 @podcast_api.route('/subscription', methods=['POST'])
 def add_subscription():
     new_podcast = Podcast()
+    user_in_session = session['user']
+    username = db.session.query(Users).filter(Users.username==user_in_session).first()
+    new_podcast.user_id = username.id
     new_podcast.name = request.json["name"]
     new_podcast.rss_feed_url = request.json["rss_feed_url"]
     db.session.add(new_podcast)
@@ -21,6 +24,8 @@ def add_subscription():
     return jsonify(success=True)
 
 @podcast_api.route('/subscription', methods=['PATCH'])
+# this is going to require an update - this is currently deleting the first instance of 
+# a subscription to a particular podcast rather than the subscription instance relative to the proper user_id
 def remove_subscription():
     pod_id = request.json["id"]
     target_podcast = db.session.query(Podcast).filter_by(id=pod_id).first()
